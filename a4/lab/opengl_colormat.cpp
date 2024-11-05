e611e0877bf0b69535a4363db65c80976307bed5
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-11-05 12:16:02 -0500

    Update opengl_colormat.cpp

--- a4/lab/opengl_colormat.cpp
+++ a4/lab/opengl_colormat.cpp
@@ -3,7 +3,7 @@
 */
 
 /*
- * Description: testing OpenGL color material features 
+ * Description: testing OpenGL color material features, diffuse material specifically
  * compile: g++ opengl_colormat.cpp -lfreeglut -lopengl32 -lglu32
  * Run: a.exe  clicking different mouse buttons to see the changes.
  * HBF
@@ -11,10 +11,17 @@
 
 #include <GL/glut.h>
 
+// diffuse reflection represents how a material reflects light in all directions from its surface.
+// means that sphere will appear brightest on the side facing the light source
+// and gradually darken on other sides, create a smooth transition of light across the surface
 
+// gray color initially
+// dynamically updated based on user interaction to change the color component
+// controls how sphere reflects light from GL_LIGHT0, change how sphere looks like under lighting
+// increasing red component makes sphere reflects more red light
 GLfloat diffuseMaterial[4] = { 0.5, 0.5, 0.5, 1.0 };
 
-void init(void) 
+void init(void)
 {
    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
@@ -22,14 +29,20 @@ void init(void)
    glClearColor (0.0, 0.0, 0.0, 0.0);
    glShadeModel (GL_SMOOTH);
    glEnable(GL_DEPTH_TEST);
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseMaterial);
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+
+   glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseMaterial); // create matte surface that don't create sharp highlights, but still showing shading based on direction of light
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular); // ideal for materials like metal, glass, that reflect light in a focused manner (hightlight)
    glMaterialf(GL_FRONT, GL_SHININESS, 25.0);
+
    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
 
+   // If disabled, then no diffuse
+   // To update the color, glColor4fv(updated diffuse material color);
    glColorMaterial(GL_FRONT, GL_DIFFUSE);
+   // to allow the use of glColor to update the diffuse material
+   // so that when glColor4fv(updatedColor); is called, it directly update the diffuse color, which affects how the sphere is shaded.
    glEnable(GL_COLOR_MATERIAL);
 }
 
@@ -94,11 +107,11 @@ int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-   glutInitWindowSize (500, 500); 
+   glutInitWindowSize (500, 500);
    glutInitWindowPosition (100, 100);
    glutCreateWindow (argv[0]);
    init ();
-   glutDisplayFunc(display); 
+   glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutMouseFunc(mouse);
    glutMainLoop();
